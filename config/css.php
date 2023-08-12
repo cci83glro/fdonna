e62d188d73440cc942f502c9b44d3a152e6d36f9
cci83glro <ciprian_condurachi@yahoo.com>
2023-08-12 19:37:11 +0200

    Latest updates

--- config/css.php
+++ config/css.php
@@ -4,6 +4,9 @@
 <link href="css/cookies-popup.css" rel="stylesheet" type="text/css" media="all"/>
 <link href="css/flexslider.css" rel="stylesheet" type="text/css"  media="all" />
 <link href="css/fonts.css" rel="stylesheet" type="text/css"  media="all" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Homemade+Apple" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400,400i,600,700,800" rel="stylesheet">
